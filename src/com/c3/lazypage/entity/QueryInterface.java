0526f59134515d371f8b9549c19121e279723ebb
fengshangbin <fengshangbin@126.com>
2020-06-28 08:04:43 +0800

    update

--- src/com/c3/lazypage/entity/QueryInterface.java
+++
@@ -1,8 +0,0 @@
-package com.c3.lazypage.entity;
-
-import java.util.ArrayList;
-
-public interface QueryInterface {
-	public Element querySelector(String regStr);
-	public ArrayList<Element> querySelectorAll(String regStr);
-}
